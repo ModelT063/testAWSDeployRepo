08eea26aa9cade330b85c6af539776788d915de9
Jenny Hsu <jennyhsu@Jennys-MacBook-Pro.local>
2023-02-24 18:35:40 -0500

    aws new user pool

--- team20/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ team20/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "team20": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminsGroupRole": "string",
+      "driversGroupRole": "string",
+      "sponsorsGroupRole": "string"
+    }
+  }
+}
