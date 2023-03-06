1f7bb4ee7a461d86fe99c134b33566274ca09d6c
Joel Pittard <91074868+ModelT063@users.noreply.github.com>
2023-03-06 00:23:12 -0500

    Update aws-exports-fixed.js

--- team20/src/pages/aws-exports-fixed.js
+++ team20/src/pages/aws-exports-fixed.js
@@ -1,4 +1,4 @@
-const awsmobile = {
+export const awsmobile = {
   "aws_project_region": "us-east-1",
   "aws_cognito_identity_pool_id": "us-east-1:b9aacf6f-4dbf-4488-a991-9c0dfa22c6fa",
   "aws_cognito_region": "us-east-1",
@@ -25,6 +25,3 @@ const awsmobile = {
       "EMAIL"
   ]
 };
-
-
-export default awsmobile;
